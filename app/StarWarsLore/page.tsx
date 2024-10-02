edd408200acdafc172baca459a92998ac5e36b86
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2024-10-02 22:19:48 +0400

    big changes

--- app/StarWarsLore/page.tsx
+++ app/StarWarsLore/page.tsx
@@ -1,9 +1,16 @@
 import CircleList from "@/components/StarWarsLore/CircleList";
 import StarWarsInfos from "@/components/StarWarsLore/StarWarsInfos";
+import Link from "next/link";
 
 const page = () => {
   return (
     <>
+      <Link
+        href="/"
+        className="absolute top-4 left-4 text-yellow-400 font-extrabold"
+      >
+        Home
+      </Link>
       <StarWarsInfos />
     </>
   );
