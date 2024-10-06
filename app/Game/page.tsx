7007ee98e612f2139b017006efd5b4538b15a973
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2024-10-06 13:33:18 +0400

    small change

--- app/Game/page.tsx
+++ app/Game/page.tsx
@@ -9,16 +9,14 @@ const page = () => {
         href="/"
         className="absolute left-2 top-0 text-white font-extrabold text-lg  rounded-lg shadow-md  transition-all duration-300 hover:scale-110"
       >
-        Go Home
+        Home
       </Link>
-      <div className="absolute text-white left-2 top-6">
-        click on the screen to start playing
-      </div>
       <div className="absolute text-white left-2 top-12">
-        use: w,a,s,d to move
-      </div>
-      <div className="absolute text-white left-2 top-16">
-        use: space to shoot
+        <div className=" text-white  ">
+          click on the screen to start playing
+        </div>
+        <div className=" text-white ">use: w,a,s,d to move</div>
+        <div className=" text-white ">use: space to shoot</div>
       </div>
       <iframe
         src="\Quick_Game2_StarWarsThing-main\Quick_Game2_StarWarsThing-main\index.html"
